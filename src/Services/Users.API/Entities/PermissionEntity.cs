1d5c9afb7c8e920ecf1eaa8fc25394d672e2235f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-23 19:41:25 -0300

    chore: permission entity

---
+++ src/Services/Users.API/Entities/PermissionEntity.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Users.API.Entities;
+
+[Table("TBL_PERMISSIONS")]
+public class PermissionEntity
+{
+    [Key]
+    [Column("ID_PERMISSION")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public required int Id { get; set; }
+    
+    [Column("TX_NAME")]
+    public required string Name { get; set; }
+    
+    [Column("TX_CODE")]
+    public required string Code { get; set; }
+    
+    [Column("TX_CREATE")]
+    public required DateTime CreatedAt { get; set; }
+    
+    [Column("TX_LAST_UPDATE")]
+    public DateTime? UpdatedAt { get; set; }
+}
